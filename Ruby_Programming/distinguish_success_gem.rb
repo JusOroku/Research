dc778b927e9957742280b3bf3956332b3a9b3093
Pannavat Terdchanakul <pannavat.terdchanakul.pp5@is.naist.jp>
2016-04-05 13:21:25 +0900

    finished distinguish gems by Ruby

--- Ruby_Programming/distinguish_success_gem.rb
+++ Ruby_Programming/distinguish_success_gem.rb
@@ -1,9 +1,21 @@
 require 'csv'
 
+
+
 def distinguish_gem(file_path)
+  outlier_files_path = "../Library_Page/outlier.csv"
+  non_outlier_files_path = "../Library_Page/non_outlier.csv"
   CSV.foreach(file_path) do |row|
     if row[1] == "1"
-      puts row[0]
+      CSV.open(outlier_files_path, "a+") do |csv|
+        csv << ["#{row[0]}"]
+      end
+    end
+
+    if row[1] == "0"
+      CSV.open(non_outlier_files_path, "a+") do |csv|
+        csv << ["#{row[0]}"]
+      end
     end
   end
 end
